1ecc6c5d11303a23fb6eabafe4482c867a37ebda
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-31 22:04:54 +0300

    added menu and run bar

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from tkinter import *
 
 compiler = Tk()
 compiler.title("MyIDE")
+
+menubar = Menu(compiler)
+
+runbar = Menu(menubar)
+runbar.add_command(label="Run")
+menubar.add_cascade(label = "Run", menu=runbar)
+compiler.config(menu=menubar)
+
 editor = Text()
 editor.pack()
 compiler.mainloop()

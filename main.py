ddccee645aadf17770aa46618f8e21c70a1f30cc
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-09-02 12:06:42 +0300

    added save as button

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from tkinter import *
 from tkinter.filedialog import asksaveasfilename
+compiler = Tk()
+compiler.title("My_IDE")
+
+
+def save_as():
+    path = asksaveasfilename(filetypes=[("python Files", "*.py")])
+    with open(path, "W") as file:
+        code = editor.get("1.0", END)
+        file.write(code)
 
 
 def run():
@@ -8,15 +17,10 @@ def run():
 
 
 menubar = Menu(compiler)
-
-
-def save_as():
-
-
 file_menu = Menu(menubar, tearoff=0)
 file_menu.add_command(label="Open", command=run)
 file_menu.add_command(label="Save", command=run)
-file_menu.add_command(label="Save AS", command=run)
+file_menu.add_command(label="Save AS", command=save_as)
 file_menu.add_command(label="Exit", command=exit)
 menubar.add_cascade(label="File", menu=file_menu)
 

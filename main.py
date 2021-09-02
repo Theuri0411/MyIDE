138aec4c72dc64a0b108ea6f71fcf6a4ec8fb1ff
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-09-02 11:44:51 +0300

    added run function to show that code will be run

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from tkinter import *
 
 compiler = Tk()
 compiler.title("MyIDE")
+
+
+def run():
+    print("Code will be run")
+
+
 menubar = Menu(compiler)
 
 runbar = Menu(menubar, tearoff=0)
-runbar.add_command(label="Run")
-menubar.add_cascade(label = "Run", menu=runbar)
+runbar.add_command(label="Run", command=run)
+menubar.add_cascade(label="Run", menu=runbar)
 compiler.config(menu=menubar)
-
 editor = Text()
 editor.pack()
 compiler.mainloop()
